1addcf4c7a5bd6af11bd86a6786519acc42c99ae
zhafiri19 <61208308+zhafiri19@users.noreply.github.com>
2025-05-20 12:52:13 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'zhafiri19.github.io/ryuki-advent-card-viewer/',
+  base: 'ryuki-advent-card-viewer/',
   plugins: [react()],
 })
